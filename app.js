478940900f415fe3ee68ab7591e6b4bd03f800c9
sofikin <sofia.kalpin@khk.ee>
2021-11-13 15:35:57 +0200

    Get tasks values from localStorage

--- app.js
+++ app.js
@@ -6,6 +6,39 @@ const delTasksBtn = document.querySelector("#del-tasks");
 form.addEventListener("submit", addTask);
 tasksList.addEventListener("click", deleteTask);
 delTasksBtn.addEventListener("click", deleteTasks);
+document.addEventListener("DOMContentLoaded", getTasksFromLocalStorage);
+
+function getTasksFromLocalStorage() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (tasksElement) {
+    // create <li> element
+    const li = document.createElement("li");
+    // define <li> css class
+    li.className = "collection-item";
+    // create text value for <li>
+    const text = document.createTextNode(tasksElement);
+    // add text value to <li>
+    li.appendChild(text);
+    // create link element
+    const a = document.createElement("a");
+    // set href attribute
+    a.setAttribute("href", "#");
+    // add css style
+    a.className = "secondary-content";
+    // add x text to link
+    a.appendChild(document.createTextNode("x"));
+    li.appendChild(a);
+    // find <ul> dom component
+    const ul = document.querySelector(".collection");
+    // add <li> to <ul>
+    ul.appendChild(li);
+  });
+}
 
 function addTask(e) {
   // input value
